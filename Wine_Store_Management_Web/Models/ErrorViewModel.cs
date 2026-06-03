c668ac68ad6e6d73a71266a53012862ebe212fe2
Viet7730922 <voqviet7206@gmail.com>
2026-06-03 23:06:01 +0700

    Add project files.

---
+++ Wine_Store_Management_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wine_Store_Management_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
